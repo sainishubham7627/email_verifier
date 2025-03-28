f7dea1155eda78ef814b82d2196a60d4ef846d45
SHUBHAMsaini <sainishubham8899@gmail.com>
2025-03-28 11:09:36 +0530

    Update NoteState.js

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 const NoteState = (props) => {
   const host = "http://localhost:5000"; // ✅ Changed port to 5000
+  const notesInitial = []
   const [notes, setNotes] = useState([]);
 
   // ✅ Fetch All Notes
